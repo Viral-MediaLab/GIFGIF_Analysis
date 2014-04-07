0bce7bd14539614f83cfbad29ba0f60c2ae6224e
Kevin Hu <kzh@Kevins-MacBook-Air-2.local>
2014-04-07 18:34:03 -0400

    Woot new scores should be here

--- scripts/db.py
+++ scripts/db.py
@@ -14,83 +14,135 @@ import numpy.random as rnd
 # They've made too many bugs hard to find. Let's add a real error logging system.
 
 class database(object):
-#--------------------Results
-
-    locid2idx = None
-    locs = None
-    studyid2prob = None
-    studs = None
-    study_prob = None
-
-#--------------------QS
     def get_score(self, image_id):
         return self.db.scores.find_one({
                 "image_id": str(image_id)
             })
 
-        # should be called internally, when adding a new image
+    def get_country_score(self, image_id):
+        return self.db.country_scores.find_one({
+                "image_id": str(image_id)
+            })
+
+    # should be called internally, when adding a new image
     def _add_score(self, image_ids, questions, countries):
-        inserted_documents = []
-        empty_score_dict = dict([(q['metric'], trueskill.get_score(trueskill.mu0, trueskill.std0)) for q in questions])
-        parameters_dict = dict([(q['metric'], {'mus':[trueskill.mu0], 'stds':[trueskill.std0]}) for q in questions])
+        score_documents = []
+        country_score_documents = []
+        empty_dict = dict([(q['metric'], 0) for q in questions])
+        empty_scores_dict = dict([(q['metric'], trueskill.get_score(trueskill.mu0, trueskill.std0)) for q in questions])
+        empty_parameters_dict = dict([(q['metric'], {'mu': trueskill.mu0, 'std': trueskill.std0}) for q in questions])
+
         for image_id in image_ids:
             document = {
+                'total_vote_count': 0,
+                'vote_counts': empty_dict,
+                'neither_counts': empty_dict,
                 'image_id': str(image_id),
-                'scores': dict([(country, empty_score_dict) for country in countries]),
-                'parameters': dict([(country, parameters_dict) for country in countries])
+                'scores': empty_scores_dict,
+                'parameters': empty_parameters_dict
             }
-            inserted_documents.append(document)
-        return Database.db.scores.insert(inserted_documents)
+            country_document = {
+                'total_vote_count': 0,
+                'vote_counts': dict([(country, empty_dict) for country in countries]),
+                'neither_counts': dict([(country, empty_dict) for country in countries]),
+                'image_id': str(image_id),
+                'scores': dict([(country, empty_scores_dict) for country in countries]),
+                'parameters': dict([(country, empty_parameters_dict) for country in countries])
+            }
+            score_documents.append(document)
+            country_score_documents.append(country_document)
+
+        Database.db.scores.insert(score_documents)
+        Database.db.country_scores.insert(country_score_documents)
+        return
 
     # Should be called internally, when update the score of an image after a vote
-    def _push_score(self, metric, score_doc, mu, std, old_mu, old_std, isDraw, country):
+    def _push_score(self, metric, score_doc, mu, std, old_mu, old_std, isDraw):
         score = trueskill.get_score(mu, std)
-        inc = 0
-        if isDraw:
-            inc = 1
+        neither_inc = 0
+        if isDraw: neither_inc = 1
 
         self.db.scores.update({'_id': score_doc['_id']}, {
-            '$set': { 'scores.' + country + '.' + metric: score },
+            '$inc': {
+                'total_vote_count': 1,
+                'vote_counts.' + metric: 1,
+                'neither_count.' + metric: neither_inc,
+            },
+            '$set': { 
+                'scores.' + metric: score,
+                'parameters.' + metric + '.mu': mu,
+                'parameters.' + metric + '.std': std
+            },
+        })
+
+    def _push_country_score(self, metric, score_doc, mu, std, old_mu, old_std, isDraw, country):
+        score = trueskill.get_score(mu, std)
+        neither_inc = 0
+        if isDraw: neither_inc = 1
+
+        self.db.country_scores.update({'_id': score_doc['_id']}, {
+            '$inc': {
+                'total_vote_count': 1,
+                'vote_counts.' + country + '.' + metric: 1,
+                'country_vote_count.' + country: 1,
+                'neither_count.' + country: neither_inc
+            },
+            '$set': { 
+                'scores.' + country + '.' + metric: score,
+                'parameters.' + country + '.' + metric + '.mu': mu,
+                'parameters.' + country + '.' + metric + '.std': std
+            },
         })
 
     # Remove redundant parameters!
     def update_scores(self, metric, winner_image_id, loser_image_id, isDraw, country):
         '''
-            Updating scores consists of several tasks:
-            1. Updating the scores for a given question
-            2. Updating the trueskill parameters
-            3. Incrementing the vote count
+        Updating scores consists of several tasks:
+            1. Updating the scores, true skill parameters, neither, and vote counts for a given emotion with and without country disaggregation
+            2. Updating the global scores and vote counts
         '''
+
         # 1. Update the scores of the two locations (images)
         winner_score_doc = self.get_score(winner_image_id)
         loser_score_doc = self.get_score(loser_image_id)
+
+
         if winner_score_doc is None:
-            print "Couldn't find a score document with image_id", winner_image_id
+            print "Couldn't find a score document with image_id:", winner_image_id
             return
         if loser_score_doc is None:
-            print "Couldn't find a score document with image_id", loser_image_id
+            print "Couldn't find a score document with image_id:", loser_image_id
             return
 
         # get the last mu and standard deviation
-        old_mu_winner = winner_score_doc['parameters'][country][metric]['mus'][-1]
-        old_std_winner = winner_score_doc['parameters'][country][metric]['stds'][-1]
-        old_mu_loser = loser_score_doc['parameters'][country][metric]['mus'][-1]
-        old_std_loser = loser_score_doc['parameters'][country][metric]['stds'][-1]
+        old_mu_winner = winner_score_doc['parameters'][metric]['mu']
+        old_std_winner = winner_score_doc['parameters'][metric]['std']
+        old_mu_loser = loser_score_doc['parameters'][metric]['mu']
+        old_std_loser = loser_score_doc['parameters'][metric]['std']
 
         # update scores using the trueskill update equations
         (mu_winner, std_winner), (mu_loser, std_loser) = trueskill.update_rating((old_mu_winner, old_std_winner), (old_mu_loser, old_std_loser), isDraw)
 
         # 2. Push scores and Trueskill parameters of the images to the db
-        self._push_score(metric, winner_score_doc, mu_winner, std_winner, old_mu_winner, old_std_winner, isDraw, country)
-        self._push_score(metric, loser_score_doc, mu_loser, std_loser, old_mu_loser, old_std_loser, isDraw, country)
+        self._push_score(metric, winner_score_doc, mu_winner, std_winner, old_mu_winner, old_std_winner, isDraw)
+
+        if country:
+            winner_country_score_doc = self.get_country_score(winner_image_id)
+            loser_country_score_doc = self.get_country_score(loser_image_id)
+
+            # get the last mu and standard deviation
+            old_mu_winner_c = winner_country_score_doc['parameters'][country][metric]['mu']
+            old_std_winner_c = winner_country_score_doc['parameters'][country][metric]['std']
+            old_mu_loser_c = loser_country_score_doc['parameters'][country][metric]['mu']
+            old_std_loser_c = loser_country_score_doc['parameters'][country][metric]['std']
+
+            (mu_winner_c, std_winner_c), (mu_loser_c, std_loser_c) = trueskill.update_rating((old_mu_winner_c, old_std_winner_c), (old_mu_loser_c, old_std_loser_c), isDraw)
+
+            self._push_country_score(metric, loser_country_score_doc, mu_loser_c, std_loser_c, old_mu_loser_c, old_std_loser_c, isDraw, country)
 
         # 3. Increment vote count for the question
         question_id = str(Database.db.questions.find_one({'metric': metric})['_id'])
         self.db.questions.update({'_id': ObjectId(question_id)}, { '$inc' : { 'num_votes': 1 }})
-
-    @property
-    def votes(self):
-        return self.db.votes
 
     @property
     def db(self):
--- scripts/run_analysis.py
+++ scripts/run_analysis.py
@@ -1,7 +1,7 @@
 '''
 Module used to calculate scores for each image and emotion pair given votes.
 
-Current version calculates scores given only votes with IPs. 
+Current version calculates scores given only votes with IPs.
 
 Usage: python run_analysis.py
 '''
@@ -13,6 +13,7 @@ sys.path.append(os.path.dirname(CURRENT_DIR))
 import pymongo
 from db import Database
 
+emotions = ['amusement', 'anger', 'contempt', 'contentment', 'disgust', 'embarrassment', 'excitement', 'fear', 'guilt', 'happiness', 'pleasure', 'pride', 'relief', 'sadness', 'satisfaction', 'shame', 'surprise']
 countries = ['A1', 'A2', 'AD', 'AE', 'AF', 'AG', 'AI', 'AL', 'AM', 'AN', 'AO', 'AP', 'AR', 'AT', 'AU', 'AW', 'AX', 'AZ', 'BA', 'BB', 'BD', 'BE', 'BG', 'BH', 'BI', 'BM', 'BN', 'BO', 'BR', 'BS', 'BY', 'BZ', 'CA', 'CH', 'CI', 'CL', 'CM', 'CN', 'CO', 'CR', 'CU', 'CV', 'CY', 'CZ', 'DE', 'DK', 'DO', 'DZ', 'EC', 'EE', 'EG', 'ES', 'ET', 'EU', 'FI', 'FO', 'FR', 'GB', 'GD', 'GE', 'GF', 'GG', 'GH', 'GI', 'GL', 'GP', 'GR', 'GT', 'HK', 'HN', 'HR', 'HT', 'HU', 'ID', 'IE', 'IL', 'IM', 'IN', 'IQ', 'IR', 'IS', 'IT', 'JE', 'JM', 'JO', 'JP', 'KE', 'KG', 'KH', 'KR', 'KW', 'KZ', 'LB', 'LI', 'LK', 'LT', 'LU', 'LV', 'MA', 'MC', 'MD', 'ME', 'MG', 'MK', 'ML', 'MM', 'MN', 'MO', 'MQ', 'MT', 'MU', 'MV', 'MX', 'MY', 'MZ', 'NA', 'NC', 'NG', 'NI', 'NL', 'NO', 'NP', 'NZ', 'OM', 'PA', 'PE', 'PF', 'PH', 'PK', 'PL', 'PR', 'PS', 'PT', 'PY', 'QA', 'RE', 'RO', 'RS', 'RU', 'RW', 'SA', 'SC', 'SD', 'SE', 'SG', 'SI', 'SK', 'SN', 'SR', 'SV', 'SY', 'TH', 'TN', 'TR', 'TT', 'TW', 'TZ', 'UA', 'UG', 'US', 'UY', 'UZ', 'VE', 'VG', 'VI', 'VN', 'VU', 'YT', 'ZA', 'ZM', 'ZW']
 
 '''
@@ -21,16 +22,18 @@ Because of this, selecting a random pair of images doesn't work correctly
 '''
 # Yield successive n-sized chunks from l.
 def chunks(l, n):
-  print len(l)
   for i in xrange(0, len(l), n):
     yield l[i:i+n]
 
-# Reset scores for all images
+# Populate empty scores docum
 def insert_scores():
   Database.db.drop_collection(Database.db.scores)
+  Database.db.drop_collection(Database.db.country_scores)
   # drop index for fast insertion
   Database.db.scores.drop_indexes()
   Database.db.scores.ensure_index("_id")
+  Database.db.country_scores.drop_indexes()
+  Database.db.country_scores.ensure_index("_id")
 
   questions = list(Database.db.questions.find())  # Need to cast into list to not run into end of cursor
   print len(questions), 'questions found in the db'
@@ -42,7 +45,6 @@ def insert_scores():
   for image_id_chunk in chunks(image_ids, chunk_size):
     result = Database._add_score(image_id_chunk, questions, countries)
     print 'Processing %s images with %s questions' % (chunk_size, len(questions))
-
   print 'Finished processing', len(image_ids), 'inserts'
 
 # Process all votes and calculate Trueskill parameters + score
@@ -50,7 +52,8 @@ def process_past_votes():
   print 'Processing past votes'
   Database.db.scores.ensure_index([('image_id', pymongo.ASCENDING)]) # is this necessary?
 
-  votes = Database.votes.find({'ip': {'$exists': True}})
+  # votes = Database.db.votes.find({'ip': {'$exists': True}})
+  votes = Database.db.votes.find()
   print votes.count(), 'un-calculated votes found in the DB'
   total_vote_count = 0
   used_vote_count = 0
@@ -65,7 +68,7 @@ def process_past_votes():
     try:
       metric = str(vote['metric'])
       question_id = str(Database.db.questions.find_one({'metric': metric})['_id'])
-      country = vote['country']
+      country = vote.get('country', None)
       used_vote_count += 1
   
       if vote['choice'] in ['left', 'equal']:
